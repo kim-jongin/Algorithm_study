278c5374b38eb5462ee0dcafebacd531a3401015
kim-jongin <whddlskss@gmail.com>
2023-01-20 01:26:09 +0900

    Lv0 추가풀이

--- algorithm_basic/level_zero.js
+++ algorithm_basic/level_zero.js
@@ -13,4 +13,24 @@ const solution = (num1, num2) => {
   return answer;
 };
 
-//Lv0_
+//Lv0_분수의 덧셈
+function solution(denum1, num1, denum2, num2) {
+    let topNum = num1*denum2 + num2*denum1
+    let botNum = num1*num2
+    let maximum = 1
+    for(let i = 1 ; i <= topNum ; i ++) {
+        if(topNum%i === 0 && botNum%i === 0) {
+            maximum = i
+        }
+    }
+    return [topNum/maximum, botNum/maximum]
+}
+
+//Lv0_배열 두배 만들기
+function solution(numbers) {
+    var answer = [];
+    for(let i = 0; i < numbers.length; i++) {
+        answer.push(numbers[i] * 2);
+    }
+    return answer;
+}

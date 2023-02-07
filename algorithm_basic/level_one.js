d2abf5c09a1c4799b1f85ee5189fd53500ddc03b
kim-jongin <whddlskss@gmail.com>
2023-02-07 23:06:24 +0900

    Lv1_평균 구하기

--- algorithm_basic/level_one.js
+++ algorithm_basic/level_one.js
@@ -2,3 +2,15 @@
 function solution(num) {
     return (num % 2 === 0 ? "Even":"Odd");
 }
+
+//Lv1_평균 구하기
+function solution(arr) {
+    let answer = 0;
+    let add = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        add += arr[i]; //배열 요소의 합을 구함
+    }
+    answer = add / arr.length;
+    return answer;
+}

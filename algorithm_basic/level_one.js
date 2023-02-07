44fa5ebbbc0902ce74e31c2230f847baaa4d1d91
kim-jongin <whddlskss@gmail.com>
2023-02-08 01:00:08 +0900

    Lv1_자릿수 더하기

--- algorithm_basic/level_one.js
+++ algorithm_basic/level_one.js
@@ -14,3 +14,15 @@ function solution(arr) {
     answer = add / arr.length;
     return answer;
 }
+
+//Lv1_자릿수 더하기
+function solution(n)
+{
+    let answer = 0;
+    n = n.toString();
+    
+    for(let i = 0; i < n.length; i++){
+        answer += parseInt(n[i])
+    }
+    return answer;
+}

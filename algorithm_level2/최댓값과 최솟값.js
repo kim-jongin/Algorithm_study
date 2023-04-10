d92f80edafa6b234fad33436f27700fd1ffef523
kim-jongin <whddlskss@gmail.com>
2023-04-10 13:08:29 +0900

    [level_2] 최댓값과 최솟값_문제풀이

--- algorithm_level2/최댓값과 최솟값.js
+++ algorithm_level2/최댓값과 최솟값.js
@@ -1,3 +1,11 @@
 // -Programmers
 // -Level_2
 // -최댓값과 최솟값
+
+function solution(s) {
+  const wordArr = s.split(' ');
+
+  const numberArr = wordArr.map((word) => Number(word));
+
+  return `${Math.min(...numberArr)} ${Math.max}(...numberArr)`;
+}

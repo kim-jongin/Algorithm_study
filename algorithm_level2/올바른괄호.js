25cdc6a5d9acac6d8a94d8a7822379d8cd12cc43
kim-jongin <whddlskss@gmail.com>
2023-04-12 13:26:08 +0900

    [level_2] 올바른괄호 풀이 설명 추가

--- algorithm_level2/올바른괄호.js
+++ algorithm_level2/올바른괄호.js
@@ -13,7 +13,10 @@ function solution(s) {
   let stack_count = 0;
   for (let i = 0; i < s.length; i++) {
     stack_count += s[i] === '(' ? 1 : -1;
+    // 문자열 s의 값이 ( 인 경우 1, 아닌경우 -1을 stack_count에 추가
     if (stack_count < 0) return false;
+    // 만약 stack_count가 0보다 클 경우 false를 출력
   }
   return stack_count === 0 ? true : false;
+  //stack_count가 0이면 true, 아닌 경우 false를 출력
 }
